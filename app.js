41b9f84e5b37238200153d0600fe9f4ddfee5585
Sonyl Nagale <sonyl.nagale@nbcuni.com>
2017-11-08 07:25:25 -0500

    Adding Create (Step 1)

--- app.js
+++ app.js
@@ -8,5 +8,26 @@ var config = {
 firebase.initializeApp(config);
     
 $(document).ready(function() {
+	var database = firebase.database();
+  
+	$('#message-form').submit(function(event) {
+		// by default a form submit reloads the DOM which will subsequently reload all our JS
+		// to avoid this we preventDefault()
+		event.preventDefault();
 
+		// grab user message input
+		var message = $('#message').val();
+
+		// clear message input (for UX purposes)
+		$('#message').val('');
+
+		// create a section for messages data in your db
+		var messagesReference = database.ref('messages');
+
+		// use the set method to save data to the messages
+		messagesReference.push({
+			message: message,
+			votes: 0
+		});
+	});
 });
